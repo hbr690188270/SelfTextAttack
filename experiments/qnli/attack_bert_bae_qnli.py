4183697f4e68430e0d0d1404303e8c7425d077c2
NormalUhr <zhangyihua1997@gmail.com>
2022-01-11 23:55:56 -0500

    change the path for qnli

--- experiments/qnli/attack_bert_bae_qnli.py
+++ experiments/qnli/attack_bert_bae_qnli.py
@@ -1,39 +1,45 @@
 # Quiet TensorFlow.
-import os
 
-import numpy as np
 # from transformers import AutoTokenizer, TFAutoModelForSequenceClassification, pipeline
 from transformers import BertTokenizer, BertForSequenceClassification
+
 import textattack
 from textattack import Attacker
 from textattack.attack_recipes import MyBAEGarg2019
-from textattack.datasets import HuggingFaceDataset
-from textattack.models.wrappers import ModelWrapper, huggingface_model_wrapper
-from textattack.models.wrappers import HuggingFaceModelWrapper
+from textattack.models.wrappers import huggingface_model_wrapper
 
-def load_dataset_rte(path = ''):
-    def process_file(file):    
+
+def load_dataset_qnli(path=''):
+    def process_file(file):
         data_list = []
-        with open(path + file,'r',encoding = 'utf-8') as f:
+        with open(path + file, 'r', encoding='utf-8') as f:
             for line in f:
-                sen1, sen2, label = line.split("\t",2)
+                sen1, sen2, label = line.split("\t", 2)
                 data_item = ((sen1, sen2), int(label))
                 data_list.append(data_item)
         return data_list
-    test_dataset = process_file("test.tsv")
-    return test_dataset
 
-directory = '/mnt/cloud/bairu/repos/std_text_pgd_attack/checkpoints/bert-base-rte'
+    test_dataset = process_file("test.tsv")
+
+    filtered_sen_list, filtered_label_list = [], []
+    test_sen_list, test_label_list = test_dataset
+    with open("~/std_text_pgd_attack/attack_set_idx/qnli_attack_idx.txt", 'r', encoding='utf-8') as f:
+        for line in f:
+            idx = int(line.strip())
+            filtered_sen_list.append(test_sen_list[idx])
+            filtered_label_list.append(test_label_list[idx])
+    return filtered_sen_list, filtered_label_list
+
+
+directory = '~/std_text_pgd_attack/checkpoints/bert-qnli/'
+dataset = load_dataset_qnli("~/std_text_pgd_attack/qnli/")
+dataset = textattack.datasets.Dataset(dataset, input_columns=['premise', 'hypothesis'])
+
 model = BertForSequenceClassification.from_pretrained(directory)
 tokenizer = BertTokenizer.from_pretrained(directory)
 wrapper_model = huggingface_model_wrapper.HuggingFaceModelWrapper(model, tokenizer)
 recipe = MyBAEGarg2019.build(wrapper_model)
 
-# dataset = HuggingFaceDataset("allocine", split="test")
-dataset = load_dataset_rte()
-dataset = textattack.datasets.Dataset(dataset, input_columns = ['premise', 'hypothesis'])
-
-attack_args = textattack.AttackArgs(num_examples = -1, log_to_txt = './log/bae_qnli_bertbase.txt', query_budget = 500)
+attack_args = textattack.AttackArgs(num_examples=-1, log_to_txt='./log/bae_qnli_bertbase.txt', query_budget=500)
 attacker = Attacker(recipe, dataset, attack_args)
 results = attacker.attack_dataset()
-
